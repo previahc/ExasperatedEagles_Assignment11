191605579158fdf39b82bd49c108e5bbea7bb7b2
Andrew Mehlman <mehlmadm@mail.uc.edu>
2024-11-20 18:55:25 -0500

    gross price code

--- ExasperatedEagles_Assignment11/dataCleanerPackage/dataCleaner.py
+++ ExasperatedEagles_Assignment11/dataCleanerPackage/dataCleaner.py
@@ -1,1 +1,6 @@
+    
 
+def clean_gross_price(self):
+        """Ensure 'Gross Price' values are rounded to 2 decimal places."""
+        self.data['Gross Price'] = pd.to_numeric(self.data['Gross Price'], errors='coerce').round(2)
+        print("Gross Price column cleaned.")
